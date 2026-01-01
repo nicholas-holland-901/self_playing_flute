47fe95246a192cd0fa65e01681fc4c85c120d516
the-nicholas-holland <the.nicholas.holland@gmail.com>
2025-12-31 18:57:53 -0600

    file readability

--- src/main.cpp
+++ src/main.cpp
@@ -4,109 +4,144 @@
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-enum Note{
-  A,
-  A_flat,
-  B,
-  B_flat,
-  C,
-  D,
-  D_flat,
-  E,
-  E_flat,
-  F,
-  G,
-  G_flat
-};
-
-#define CLOSED 150;
-#define HALF 200;
-#define OPEN 250;
+#define CLOSED 190;
+#define OPEN 220;
+#define CLOSED_R 240;
+#define OPEN_R 210;
 
 int thumb_0 = CLOSED;
 int index_1 = CLOSED;
 int middle_2 = CLOSED;
 int ring_3 = CLOSED;
-int index_4 = CLOSED;
-int middle_5 = CLOSED;
-int ring_6 = CLOSED;
-int pinky_7 = CLOSED;
+int pinky_4 = CLOSED;
+int pointer_5 = CLOSED;
+int middle_6 = CLOSED;
+int ring_7 = CLOSED;
+int pinky_8 = CLOSED;
 
-void set_note(Note n, int octave);
+void set_note(int n);
+void open_all();
+void close_all();
+
+#define song_size 85
+int timer[song_size] = {112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 140, 140, 140, 140, 140, 140, 140, 222, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 200, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112, 112};
+int notes[song_size] = {83, 84, 83, 81, 79, 78, 76, 75, 76, 79, 83, 75, 76, 79, 83, 76, 71, 70, 75, 76, 78, 79, 82, 83, 84, 88, 83, 79, 76, 72, 71, 67, 66, 78, 67, 79, 68, 80, 76, 88, 93, 91, 90, 88, 87, 84, 83, 88, 86, 84, 83, 81, 79, 78, 76, 83, 81, 79, 78, 76, 75, 76, 79, 83, 75, 76, 79, 83, 76, 66, 70, 75, 78, 79, 83, 88, 91, 93, 91, 88, 83, 79, 78, 75, 70};
+int iter = 0;
 
 void setup() {
   Serial.begin(9600);
   pwm.begin();
-  pwm.setPWMFreq(60);
+  pwm.setPWMFreq(50);
 }
+
+int test = 62;
 
 void loop() {
-  pwm.setPWM(0, 0, thumb_0);
+  // open_all();
+  set_note(test);
+  pwm.setPWM(0, 0, thumb_0 - 10);
   pwm.setPWM(1, 0, index_1);
-  pwm.setPWM(2, 0, middle_2);
-  pwm.setPWM(3, 0, ring_3);
-  pwm.setPWM(4, 0, index_4);
-  pwm.setPWM(5, 0, middle_5);
-  pwm.setPWM(6, 0, ring_6);
-  pwm.setPWM(7, 0, pinky_7);
+  pwm.setPWM(2, 0, middle_2 - 30);
+  pwm.setPWM(4, 0, ring_3 - 45);
+  pwm.setPWM(3, 0, pinky_4 - 50);
+  pwm.setPWM(5, 0, pointer_5 - 50);
+  pwm.setPWM(6, 0, middle_6 + 20);
+  pwm.setPWM(7, 0, ring_7 + 40);
+  pwm.setPWM(8, 0, pinky_8 + 10);
+
+  if (timer[iter] != 0){
+    delay((int)(timer[iter] * 4));
+  } else{
+    delay(1000);
+  }
+  iter++;
+  if (iter > song_size - 1){
+    iter = 0;
+    delay(1000);
+  }
+  test++;
+  if (test > 90){
+    test = 62;
+  }
 }
 
-void set_note(Note n, int octave){
-  // thumb_0 open for Db5, D5, Eb5
-  if (((n == D_flat || n == D || n == E_flat) && octave == 5)){
-    thumb_0 = OPEN;
-  // thumb_0 half-closed for notes above Eb5
-  } else if (((n != C && n != D_flat && n != D && n != E_flat) && octave == 5) || octave == 6){
-    thumb_0 = HALF;
+void set_note(int n){
+  // thumb_0 open for C5, Db5, C6, Db6, G6, Ab6, C7
+  if (n == 72 || n == 73 || n == 84 || n == 85 || n == 91 || n == 92 || n == 96){
+    thumb_0 = OPEN_R;
   } else{
-    thumb_0 = CLOSED;
+    thumb_0 = CLOSED_R;
   }
-  // index_1 open for C5, D5, Eb5, Eb6
-  if (((n == C || n == D) && octave == 5) || (n == E_flat && (octave == 5 || octave == 6))){
+  // index_1 open for Db5, D5, Db6, D6, Ab6, A6, Bb6
+  if (n == 73 || n == 74 || n == 85 || n == 86 || n == 92 || n == 93 || n == 94){
     index_1 = OPEN;
   } else{
     index_1 = CLOSED;
   }
-  // middle_2 open for Bb4, B4, C6, Db6, D6
-  if (((n == B_flat || n == B) && octave == 4) || ((n == C || n == D_flat || n == D) && octave == 6)){
+  // middle_2 open for Bb4, B4, C5, Db5, Bb5, B5, C6, Db6, F6, Gb6, Bb6, B6 
+  if (n == 70 || n == 71 || n == 72 || n == 73 || n == 82 || n == 83 || n == 84 || n == 85 || n == 89 || n == 90 || n == 94 || n == 95){
     middle_2 = OPEN;
   } else{
     middle_2 = CLOSED;
   }
-  // ring_3 open for Ab4, A4, B4, C5, Db5, D5, Ab5, A5, Bb5, B5, C6 
-  if (((n == A_flat || n == A || n == B) && octave == 4) || ((n == C || n == D_flat || n == D || n == A_flat || n == A || n == B_flat || n == B) && octave == 5) || (n == C && octave == 6)){
+  // ring_3 open for A4, Bb4, B4, C5, Db5, A5, Bb5, B5, C6, Db6, E6, A6, Bb6
+  if (n == 69 || n == 70 || n == 71 || n == 72 || n == 73 || n == 81 || n == 82 || n == 83 || n == 84 || n == 85 || n == 88 || n == 93 || n == 94){
     ring_3 = OPEN;
   } else{
     ring_3 = CLOSED;
   }
-  // index_4 open for Gb4, G4, A4, B4, C5, Db5, D5, Gb5, G5, A5, Bb5, Eb6
-    if (((n == G_flat || n == G || n == A || n == B) && octave == 4) || ((n == C || n == D_flat || n == D || n == G_flat || n == G || n == A || n == B_flat) && octave == 5) || (n == E_flat && octave == 6)){
-    index_4 = OPEN;
+  // pinky_4 closed for Ab4, Ab5, Eb6, Ab6, C7
+    if (n == 68 || n == 80 || n == 87 || n == 92 || n == 96){
+    pinky_4 = CLOSED;
   } else{
-    index_4 = CLOSED;
+    pinky_4 = OPEN;
   }
-  // middle_5 open for F4, G4, A4, Bb4, B4, C5, Db5, D5, F5, G5, Ab5, A5, Db6, D6
-      if (((n == F || n == G || n == A || n == B_flat || n == B) && octave == 4) || ((n == C || n == D_flat || n == D || n == F || n == G || n == A_flat || n == A) && octave == 5) || ((n == D_flat || n == D) && octave == 6)){
-    middle_5 = OPEN;
+  // pointer_5 closed for D4, Eb4, E4, F4, Bb4, D5, Eb5, E5, F5, Bb5, Eb6, E6, F6, A6, Bb6, C7
+      if (n == 62 || n == 63 || n == 64 || n == 65 || n == 70 || n == 74 || n == 75 || n == 76 || n == 77 || n == 82 || n == 87 || n == 88 || n == 89 || n == 93 || n == 94 || n == 96){
+    pointer_5 = CLOSED;
   } else{
-    middle_5 = CLOSED;
+    pointer_5 = OPEN;
   }
-  // ring_6 closed for C4, Db4, D4, F4, Gb4, Eb5, F5, Bb5, Db6, D6, Eb6
-  if (((n == C || n == D_flat || n == D || n == B) && octave == 4) || ((n == C || n == D_flat || n == D || n == G_flat || n == G || n == A || n == B_flat) && octave == 5) || (n == E_flat && octave == 6)){
-    ring_6 = CLOSED;
-  // ring_6 half-closed for Eb4, Ab4
-  } else if ((n == E_flat || n ==  A_flat) && octave == 4){
-    ring_6 = HALF;
+  // middle_6 closed for D4, Eb4, E4, D5, Eb5, E5, Eb6, E6
+  if (n == 62 || n == 63 || n == 64 || n == 74 || n == 75 || n == 76 || n == 87 || n == 88){
+    middle_6 = CLOSED;
   } else{
-    ring_6 = OPEN;
+    middle_6 = OPEN;
   }
-  // pinky_7 closed for C4, F4, Db6, D6
-  if (((n == C || n == F) && octave == 4) || ((n == D_flat || n == D) && octave == 6)){
-    pinky_7 = CLOSED;
-  // pinky_7 half-closed for Db4, Bb5
-  } else if ((n == D_flat && octave == 4) || (n == B_flat && octave == 5)){
-    pinky_7 = HALF;
+  // ring_7 closed for D4, Eb4, Gb4, D5, Eb5, Gb5, Eb6, Gb6
+  if (n == 62 || n == 63 || n == 66 || n == 74 || n == 75 || n == 78 || n == 87 || n == 90){
+    ring_7 = CLOSED;
+  }  else{
+    ring_7 = OPEN;
+  }
+  // pinky_8 open for D4, D5, C7
+  if (n == 62 || n == 74 || n == 96){
+    pinky_8 = OPEN_R;
   } else{
-    pinky_7 = OPEN;
+    pinky_8 = CLOSED_R;
   }
+}
+
+void open_all(){
+  thumb_0 = CLOSED;
+  index_1 = OPEN;
+  middle_2 = OPEN;
+  ring_3 = OPEN;
+  pinky_4 = OPEN;
+  pointer_5 = OPEN;
+  middle_6 = OPEN;
+  ring_7 = OPEN;
+  pinky_8 = CLOSED;
+}
+
+void close_all(){
+  thumb_0 = OPEN;
+  index_1 = CLOSED;
+  middle_2 = CLOSED;
+  ring_3 = CLOSED;
+  pinky_4 = CLOSED;
+  pointer_5 = CLOSED;
+  middle_6 = CLOSED;
+  ring_7 = CLOSED;
+  pinky_8 = OPEN;
 }
---
+++ src/midi_file_reader.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+
+
+int main(){
+    std::string fileName = "bird_song.mid";
+    std::ifstream midiFile;
+    midiFile.open(fileName, std::ios::binary | std::ios::in | std::ios::ate);
+    if (!midiFile){
+        std::cout << "ERROR OPENING FILE";
+        return 1;
+    }
+
+    char* mem_block;
+    size_t size = midiFile.tellg();
+    mem_block = new char[size];
+    midiFile.seekg(0, std::ios::beg);
+    midiFile.read(mem_block, size);
+    midiFile.close();
+
+    unsigned char* mem_block_u = reinterpret_cast<unsigned char *>(mem_block);
+
+    int size_s = 0;
+    // Note
+    for (int i = 0; i < size; i++){
+        if ((int)mem_block_u[i] == 144){
+            printf("%d, ", mem_block_u[i + 1]);
+            size_s++;
+        }
+    }
+    printf("\n\n");
+    // Timing
+    for (int i = 0; i < size; i++){
+        if((int)mem_block_u[i] == 144){
+            printf("%d, ", mem_block_u[i + 4]);
+        }
+    }
+    printf("\nArray size: %d\n", size_s);
+   
+    delete[] mem_block;
+}
